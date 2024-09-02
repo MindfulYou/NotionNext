6e3ac973675c7b93969de0c5ef919119676f9a50
MindfulYou <chuxueyou@gmail.com>
2024-09-02 16:57:02 +0800

    Update Announcement.js

--- themes/simple/components/Announcement.js
+++ themes/simple/components/Announcement.js
@@ -6,14 +6,8 @@ const Announcement = ({ post, className }) => {
   if (!post) {
     return <></>
   }
-  return (
-    <>
-      {post && (
-        <div id="announcement-content" className={`px-3 ${className} centered-text`}>
-          <NotionPage post={post} />
-        </div>
-      )}
-    </>
-  )
+  return <>{post && (<div id="announcement-content" className={`px-3 ${className} centered-text`}>
+        <NotionPage post={post} />
+    </div>)} </>
 }
 export default Announcement
